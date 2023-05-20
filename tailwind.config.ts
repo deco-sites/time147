a38d3678fb259f335a12bd7de20d6254f5d07c9b
Romulo Rosa <romulo.frontend@gmail.com>
2023-05-20 18:57:09 -0300

    change color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export const theme = {
   "warning": "hsl(43 100% 95%)",
   "error": "hsl(9 100% 95%)",
   "info": "hsl(220 100% 97%)",
-  "text-primary-content" : "hsl(0, 0%, 100%)",
+  "text-primary-content": "hsl(0, 0%, 100%)",
 
   "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
   "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
